42d931c48b9918c6f8a541c9e7753566b81c5a53
Newton KELVIN <newton.kelvin17@students.dkut.ac.ke>
2020-06-28 18:25:40 +0300

    --- Auto-Git Commit ---

---
+++ test.py
@@ -0,0 +1,17 @@
+import requests
+
+url = "https://priaid-symptom-checker-v1.p.rapidapi.com/symptoms"
+
+querystring = {"format": "json", "language": "en-gb"}
+
+headers = {
+    'x-rapidapi-host': "priaid-symptom-checker-v1.p.rapidapi.com",
+    'x-rapidapi-key': "8384a30b45mshd82923e01e66c7fp1d4035jsn9cc1bd51e260"
+ }
+
+response = requests.request(
+     "GET", url, headers=headers, params=querystring)
+
+
+
+print(response.text)
